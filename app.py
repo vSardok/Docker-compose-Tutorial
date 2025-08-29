9a05c87dd794012fe412dbb9b3f307c4ae33ab5d
vSardok <luispelayo16@hotmail.com>
2025-08-28 19:11:51 -0600

    Step 6

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return f'Hello World! I have been seen {count} times.\n'
+    return f'Hello from Docker! I have been seen {count} times.\n'
